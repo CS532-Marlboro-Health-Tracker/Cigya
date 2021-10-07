53d22b133820b501ab6a43da3d2353e5f95480ab
Cade Walton <waltoncade@gmail.com>
2021-10-06 17:56:13 -0700

    Removed unnecessary environment stuff

--- tests/test_action.py
+++ tests/test_action.py
@@ -4,6 +4,9 @@ class TestStringMethods(unittest.TestCase):
 
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
+    
+    def test_upper_fail(self):
+        self.assertEqual('foo', 'foo')
 
 if __name__ == '__main__':
     unittest.main()
