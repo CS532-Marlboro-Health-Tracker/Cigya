9f75245f4f3904a3764aed980513ffbd62aaafa7
Vik Handa <vikram.handa123@gmail.com>
2021-10-27 16:30:02 -0700

    Initial Commit

--- cigya/opening_ui.py
+++ cigya/opening_ui.py
@@ -4,9 +4,15 @@ import sys
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__() # Call the inherited classes __init__ method
-        uic.loadUi('cigya/ui/main.ui', self) # Load the .ui file
+        uic.loadUi('cigya/ui/er.ui', self) # Load the .ui file
         self.show() # Show the GUI
+
+    #function that may be able to store data fields?
+    def import_data(self):
+        #name, done1 = QtWidgets.QInputDialog.getText(self, 'Input Dialog', 'Enter your name.')
+        pass
 
 app = QtWidgets.QApplication(sys.argv)
 window = Ui()
 app.exec_()
+
