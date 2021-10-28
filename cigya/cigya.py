1ef0734818fc5a8849b7e6dd42cc09c5b82ea1ea
Cade Walton <waltoncade@gmail.com>
2021-10-27 18:16:12 -0700

    Login page working

--- cigya/cigya.py
+++ cigya/cigya.py
@@ -3,21 +3,34 @@ from PyQt5 import QtWidgets, uic
 import sys
 import sqlite3
 
-class Ui(QtWidgets.QMainWindow):
+class loginUI(QtWidgets.QMainWindow):
     def __init__(self):
-        super(Ui, self).__init__() # Call the inherited classes __init__ method
+        super(loginUI, self).__init__() # Call the inherited classes __init__ method
         uic.loadUi('cigya/ui/login.ui', self) # Load the .ui file
         self.conn = sqlite3.connect('cigya.db')
+        self.main = mainUI()
         self.signalSetup()
-        self.show() # Show the GUI
+        self.show()
 
     def signalSetup(self):
         self.loginBtn = self.findChild(QtWidgets.QPushButton, 'loginBtn')
         self.loginBtn.clicked.connect(lambda: login.login_clicked(self))
 
         self.usernameInput = self.findChild(QtWidgets.QLineEdit, 'usernameInput')
+        self.passwordInput = self.findChild(QtWidgets.QLineEdit, 'passwordInput')
+
+class mainUI(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(mainUI, self).__init__() # Call the inherited classes __init__ method
+        uic.loadUi('cigya/ui/main.ui', self) # Load the .ui file
+        self.signalSetup()
+
+    def signalSetup(self):
+        pass
+
 
 if __name__ == '__main__':
+    print("Trix Tesimon,Physician,dgXcWH")
     app = QtWidgets.QApplication(sys.argv)
-    window = Ui()
+    window = loginUI()
     app.exec_()
--- cigya/functions/login.py
+++ cigya/functions/login.py
@@ -2,7 +2,10 @@
 def login_clicked(self):
     cursor = self.conn.execute("SELECT name, password FROM user")
     for row in cursor:
-        print("Name = {}, Password = {}".format(row[0], row[1]))
-    # print(cursor)
-    # print(self.usernameInput.text())
+        if row[0] == self.usernameInput.text():
+            if row[1] == self.passwordInput.text():
+                self.main.show()
+                print("Login Under:\nName = {}, Password = {}".format(row[0], row[1]))
+                return
+    print("Invalid Username/Password Combination")
 
