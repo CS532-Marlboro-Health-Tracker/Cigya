8e7d5256a4f01fcfd7c052bf839cbc5e3083d8af
Cade Walton <waltoncade@gmail.com>
2021-11-16 16:12:14 -0800

    Unit Tests Working

--- tests/test_action.py
+++ tests/test_action.py
@@ -1,12 +1,16 @@
 import unittest
+import sqlite3
+from platform import system
 
 class TestStringMethods(unittest.TestCase):
+    conn = sqlite3.connect('../db/cigya.db') if (system() == "Darwin") else sqlite3.connect('db/cigya.db')
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def test_users(self):
+        en_table = ['Electronic Patient Record', 'Physician Scheduler', 'Lab Order Tracking',
+                'Pharmacy Order Tracking', 'Insurance Billing', 'Equipment Inventory and Maintenance']
+        cursor = self.conn.execute("SELECT * FROM subsystem WHERE employee_id=:id", {"id":1})
+        for i, row in enumerate(cursor):
+            self.assertEqual(row[1],en_table[i])
     
-    def test_upper_fail(self):
-        self.assertEqual('foo', 'foo')
-
 if __name__ == '__main__':
     unittest.main()
